98518bc78884958ff2b4f1f979793818052640ff
aadilahammad86 <aadilahammad86@gmail.com>
2026-05-06 14:17:27 +0400

    feat: added prometheus metrics in the index file and added prom-client package

--- api/src/index.js
+++ api/src/index.js
@@ -1,112 +1,121 @@
-  // index.js
-  require("dotenv").config();
+// index.js
+require("dotenv").config();
 
-  function withTimeout(promise, ms) {
-    return Promise.race([
-      promise,
-      new Promise((_, reject) =>
-        setTimeout(() => reject(new Error("Timeout")), ms)
-      ),
-    ]);
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics({ register: client.register });
+
+function withTimeout(promise, ms) {
+  return Promise.race([
+    promise,
+    new Promise((_, reject) =>
+      setTimeout(() => reject(new Error("Timeout")), ms)
+    ),
+  ]);
+}
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+const redis = require("redis");
+const redisClient = redis.createClient({
+  url: `redis://${process.env.REDIS_HOST || "localhost"}:${process.env.REDIS_PORT || 6379}`,
+});
+redisClient.on("error", (err) => console.error("Redis Client Error", err));
+async function redisConnect() {
+  try {
+    await redisClient.connect();
+    console.log("Connected to Redis");
+
+    app.listen(PORT, () => {
+      console.log(`API running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to connect to Redis:", err.message);
+    process.exit(1);
   }
+}
+redisConnect();
 
-  const { Pool } = require("pg");
+const express = require("express");
 
-  const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-  });
+const app = express();
 
-  const redis = require("redis");
-  const redisClient = redis.createClient({
-    url: `redis://${process.env.REDIS_HOST || "localhost"}:${process.env.REDIS_PORT || 6379}`,
-  });
-  redisClient.on("error", (err) => console.error("Redis Client Error", err));
-  async function redisConnect() {
-    try {
-      await redisClient.connect();
-      console.log("Connected to Redis");
+app.use(express.json());
 
-      app.listen(PORT, () => {
-        console.log(`API running on port ${PORT}`);
-      });
-    } catch (err) {
-      console.error("Failed to connect to Redis:", err.message);
-      process.exit(1);
+app.get("/users", async (req, res) => {
+  try {
+    const result = await withTimeout(pool.query("SELECT NOW()"), 5000);
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    if (err.message === "Timeout") {
+      res.status(504).send("DB query timed out");
+    } else {
+      res.status(500).send("DB error");
     }
   }
-  redisConnect();
+});
 
-  const express = require("express");
+app.get("/health", (req, res) => {
+  res.json({ status: "ok" });
+});
 
-  const app = express();
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
 
-  app.use(express.json());
+app.get("/queue", async (req, res) => {
+  try {
+    const length = await withTimeout(redisClient.lLen("jobs"), 2000);
+    res.json({ queueLength: length });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Queue unavailable" });
+  }
+});
 
-  app.get("/users", async (req, res) => {
-    try {
-      const result = await withTimeout(pool.query("SELECT NOW()"), 5000);
-      res.json(result.rows);
-    } catch (err) {
-      console.error(err);
-      if (err.message === "Timeout") {
-        res.status(504).send("DB query timed out");
-      } else {
-        res.status(500).send("DB error");
-      }
+app.get("/health/worker", async (req, res) => {
+  try {
+    const alive = await redisClient.get("worker_alive");
+
+    if (!alive) {
+      return res.status(503).json({ status: "Worker not alive" });
     }
-  });
 
-  app.get("/health", (req, res) => {
-    res.json({ status: "ok" });
-  });
+    res.json({ status: "Worker alive" });
 
-  app.get("/", (req, res) => {
-    res.send("API is running");
-  });
-
-  app.get("/queue", async (req, res) => {
-    try {
-      const length = await withTimeout(redisClient.lLen("jobs"), 2000);
-      res.json({ queueLength: length });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ error: "Queue unavailable" });
-    }
-  });
-
-  app.get("/health/worker", async (req, res) => {
-    try {
-      const alive = await redisClient.get("worker_alive");
-      
-      if (!alive) {
-        return res.status(503).json({ status: "Worker not alive" });
-      }
-
-      res.json({ status: "Worker alive" });
-      
-    } catch (err) { 
-      res.status(500).json({ error: "Redis unavailable" });
-    }
-  });
+  } catch (err) {
+    res.status(500).json({ error: "Redis unavailable" });
+  }
+});
 
 
-  app.post("/job", async (req, res) => {
-    try {
-      await withTimeout(
-        redisClient.rPush("jobs", JSON.stringify({ task: "demo" })),
-        2000 // 2 seconds
-      );
+app.post("/job", async (req, res) => {
+  try {
+    await withTimeout(
+      redisClient.rPush("jobs", JSON.stringify({ task: "demo" })),
+      2000 // 2 seconds
+    );
 
-      res.json({ message: "Job added to queue" });
+    res.json({ message: "Job added to queue" });
 
-    } catch (err) {
-      console.error("Redis error:", err.message);
-      res.status(500).json({ error: "Queue unavailable" });
-    }
-  });
+  } catch (err) {
+    console.error("Redis error:", err.message);
+    res.status(500).json({ error: "Queue unavailable" });
+  }
+});
 
-  const PORT = process.env.PORT || 5000;
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+const PORT = process.env.PORT || 5000;
